6fc64de8aaca4de0c075f830355f351f66cf10f9
vuongb2 <brandonvvuong@gmail.com>
2023-08-12 22:32:02 -0700

    updating before switching branches

--- src/pages/Forum.js
+++ src/pages/Forum.js
@@ -1,15 +1,19 @@
-import Navbar from '../components/Navbar'
-import Searchbar from '../components/Searchbar'
-import Filters from '../components/Filters'
-import CreatePost from '../components/CreatePost'
-import Posted from '../components/Posted'
-import './Forum.css'
-import Footer from '../components/Footer.js'
+
+import Searchbar from '../components/Searchbar';
+import Filters from '../components/Filters';
+import CreatePost from '../components/CreatePost';
+import Posted from '../components/Posted';
+import './Forum.css';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore'
+
 
 export default function Forum() {
     return(
         <div className="forumPage">
-            <Navbar/>;
             <div className="innerContent">
                 <Searchbar/>
                 <Filters />
@@ -17,8 +21,6 @@ export default function Forum() {
                 <Posted />
             </div>
             <div className="outerContent">
-                {/*FOOTER*/}
-                <Footer/>
             </div>
         </div>
     );
